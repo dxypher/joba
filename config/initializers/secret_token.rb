6d59fc69d749a811e38005a9779acf57592a7e2c
Nikhil <hira1sem@gmail.com>
2012-04-29 17:46:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joba::Application.config.secret_token = '463cf6d693bcc75cd19fb3d1e30fba8f0225a66ff969934d9e5d4bcce14680babcb8f6b1d14d6b6dae68a716a01d20888e14b2d4f7ab544205dce2c5058aaa11'
