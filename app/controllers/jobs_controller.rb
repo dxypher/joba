97da1eb765c13564de2de4433c50c90272c411b1
Nikhil <hira1sem@gmail.com>
2012-05-01 15:33:33 -0500

    Complete CRUD for Jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,41 @@
 class JobsController < ApplicationController
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new(params[:job])
+		@job.save
+		redirect_to '/jobs'
+	end
+
 	def list
 		@jobs = Job.all
 	end
 
 	def show
 		@job = Job.find_by_id(params[:id])
+	end
+
+	def edit
+		@job = Job.find_by_id(params[:id])
+	end
+
+	def update
+		@job = Job.find_by_id(params[:id])
+		@job.update_attributes(params[:job])
+		redirect_to(:action => 'show')
+	end
+
+	def delete
+		@job = Job.find_by_id(params[:id])
+	end
+
+	def destroy
+		@job = Job.find_by_id(params[:id]).destroy
+
+		redirect_to ('/jobs')
+		
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Joba::Application.routes.draw do
   root to: 'static_pages#home'
 
   get '/dashboard', :controller => 'dashboard', :action => 'index'
+
   get '/jobs', :controller => 'jobs', :action => 'list'
+
+  get '/jobs/new', :controller => 'jobs', :action => 'new'
   get '/jobs/:id', :controller => 'jobs', :action => 'show'
+
+  post '/jobs', :controller => 'jobs', :action => 'create'
+  get '/jobs/edit/:id', :controller => 'jobs', :action => 'edit'
+  
+  put '/jobs/:id', :controller => 'jobs', :action => "update"
+
+  get '/jobs/delete/:id', :controller => "jobs", :action => "delete"
+  delete 'jobs/:id', :controller => 'jobs', :action => 'destroy'
+
 end
